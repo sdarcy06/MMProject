c36ea40b59ab61f1e15bda2dc6bfef2c74cfa33f
sdarcy06 <sdarcy06@hotmail.com>
2016-03-03 23:45:56 +0000

    Initial commit

---
+++ app/src/main/java/com/example/sean/mmproject/music.java
@@ -0,0 +1,16 @@
+package com.example.sean.mmproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sean on 03/03/2016.
+ */
+public class music extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.music);
+    }
+}
